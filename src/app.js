d1223ae6e83817781728a4fae9fc79f6219cf1cf
Grego12098 <jgregory12098@gmail.com>
2023-06-20 14:58:26 +0100

    added cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import express from "express";
+import cors from "cors";
 import usersRoutes from "./routes/usersRoutes.js";
 import entryRoutes from "./routes/entryJournalRoutes.js";
 
 const app = express(); // create the app with express
+
+// Enable Cors middleware for frontend access
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://wonderful-paletas-0c1299.netlify.app/'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 
 // middleware para que express entienda los json
 app.use(express.json());

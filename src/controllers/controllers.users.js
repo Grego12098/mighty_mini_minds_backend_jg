3bdd95729b105c94bd4d4e9eb3aff135e233e779
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-06-28 09:26:18 +0100

    latest2

--- src/controllers/controllers.users.js
+++ src/controllers/controllers.users.js
@@ -44,10 +44,10 @@ export const createUser = async (req, res) => {
     } = req.body;
 
     //  Check if username already exists in the database
-    //  const existingUser = await users.findOne({ username : req.body.username });
-    //   if (existingUser) {
-    //   return res.status(400).json({ message: 'Username is already taken' });
-    // }
+      const existingUser = await users.findOne({ username : req.body.username });
+       if (existingUser) {
+       return res.status(400).json({ message: 'Username is already taken' });
+    }
 
   // hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
